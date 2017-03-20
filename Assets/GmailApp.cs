711f9bed0e7c42204dac33eaa7fecce1039cf11e
Daniel Banowetz <dbanowetz1@gmail.com>
2017-03-20 19:49:33 -0400

    Initial Commit

---
+++ Assets/GmailApp.cs
@@ -0,0 +1,404 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.ComponentModel;
+using EASendMail;
+
+public class GmailApp : MonoBehaviour {
+    
+    const string clientID = "39453173011-lmomtla80onjhdn272igqg68fs9n941j.apps.googleusercontent.com";
+    const string clientSecret = "L6EPLGeFkk0AlTBROHAoGa35";
+    const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+    const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+    const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+    const string userID = "dbanowetz1@gmail.com";
+    string access_token = "";
+    [SerializeField]
+    private InputField Body;
+    [SerializeField]
+    private InputField To;
+    [SerializeField]
+    private InputField From;
+    [SerializeField]
+    private InputField Subject;
+    [SerializeField]
+    private Canvas loginPage;
+    [SerializeField]
+    private Canvas menuPage;
+    [SerializeField]
+    private Canvas sendEmailPage;
+    [SerializeField]
+    private Button SendEmailButton;
+    [SerializeField]
+    private Button loginButton;
+    [SerializeField]
+    private Button emailButton;
+    [SerializeField]
+    private Button messagesButton;
+    [SerializeField]
+    NewBehaviourScript notifySystem;
+    private bool resultEmailSucess;
+    private bool triggerResultEmail;
+
+    void Awake()
+    {
+       
+    }
+    // Use this for initialization
+    void Start () {
+        bindEventsandFunctions();
+       
+    }
+    private void bindEventsandFunctions()
+    {
+        //Bind OAUTH login function to button
+        var loginButtonEvent = new Button.ButtonClickedEvent();
+        loginButtonEvent.AddListener(login);
+        loginButton.onClick = loginButtonEvent;
+
+        //Bind the function to check messages to the message menu button.
+        var messagesEvent = new Button.ButtonClickedEvent();
+        messagesEvent.AddListener(getMessages);
+        messagesButton.onClick = messagesEvent;
+        //Bind the function that sends an email to a menu button
+        var sendEmailEvent = new Button.ButtonClickedEvent();
+        sendEmailEvent.AddListener(switchCanvasForSendingEmail);
+        SendEmailButton.onClick = sendEmailEvent;
+
+
+    }
+    //TODO:Find a way to parse through the return string so I can display the information as a notification
+    private void getMessages()
+    {
+        handleMessages(access_token);
+    }
+    private void handleMessages(string access_token)
+    {
+        string userinfoRequestURI = string.Format("https://www.googleapis.com/gmail/v1/users/" + userID + "/messages");
+
+        // sends the request
+        HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(userinfoRequestURI);
+
+        userinfoRequest.Method = "GET";
+        userinfoRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
+        userinfoRequest.ContentType = "application/x-www-form-urlencoded";
+        userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+        // gets the response
+        WebResponse userinfoResponse = userinfoRequest.GetResponse();
+        using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream()))
+        {
+            // reads response body
+            string userinfoResponseText = userinfoResponseReader.ReadToEnd();
+            //     Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(userinfoResponseText);
+
+            notifySystem.AddNewNotification("userinfoCall", userinfoResponseText);
+        }
+    }
+    #region Login
+    private void login()
+    {
+        string state = randomDataBase64url(32);
+        string code_verifier = randomDataBase64url(32);
+        string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+        const string code_challenge_method = "S256";
+
+        // Creates a redirect URI using an available port on the loopback address.
+        string redirectURI = string.Format("http://{0}:{1}/", IPAddress.Loopback, GetRandomUnusedPort());
+        // output("redirect URI: " + redirectURI);
+
+        // Creates an HttpListener to listen for requests on that redirect URI.
+        var http = new HttpListener();
+        http.Prefixes.Add(redirectURI);
+        // output("Listening..");
+        http.Start();
+
+        // Creates the OAuth 2.0 authorization request.
+        string authorizationRequest = string.Format("{0}?response_type=code&scope=https://mail.google.com&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+            authorizationEndpoint,
+            System.Uri.EscapeDataString(redirectURI),
+            clientID,
+            state,
+            code_challenge,
+            code_challenge_method);
+
+        // Opens request in the browser.
+        System.Diagnostics.Process.Start(authorizationRequest);
+
+        // Waits for the OAuth authorization response.
+        var context = http.GetContext();
+
+        #region MyRegion
+        // Brings this app back to the foreground.
+
+
+        // Sends an HTTP response to the browser.
+        var response = context.Response;
+        string responseString = string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
+        var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        var responseOutput = response.OutputStream;
+        responseOutput.Write(buffer, 0, buffer.Length);
+        //responseOutput.WriteAsync(buffer, 0, buffer.Length)
+       
+            responseOutput.Close();
+            http.Stop();
+
+
+        // Checks for errors.
+        if (context.Request.QueryString.Get("error") != null)
+        {
+            return;
+        }
+        if (context.Request.QueryString.Get("code") == null
+            || context.Request.QueryString.Get("state") == null)
+        {
+            return;
+        }
+
+        //// extracts the code
+        var code = context.Request.QueryString.Get("code");
+        var incoming_state = context.Request.QueryString.Get("state");
+
+        //// Compares the receieved state to the expected value, to ensure that
+        //// this app made the request which resulted in authorization.
+        if (incoming_state != state)
+        {
+            return;
+        }
+
+        //// Starts the code exchange at the Token Endpoint.
+        performCodeExchange(code, code_verifier, redirectURI);
+        #endregion
+
+    }
+    void performCodeExchange(string code, string code_verifier, string redirectURI)
+    {
+
+        // builds the  request
+        string tokenRequestURI = "https://www.googleapis.com/oauth2/v4/token";
+        string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&client_secret={4}&scope=&grant_type=authorization_code",
+            code,
+            System.Uri.EscapeDataString(redirectURI),
+            clientID,
+            code_verifier,
+            clientSecret
+            );
+        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+        // sends the request
+        HttpWebRequest tokenRequest = (HttpWebRequest)WebRequest.Create(tokenRequestURI);
+        tokenRequest.Method = "POST";
+        tokenRequest.ContentType = "application/x-www-form-urlencoded";
+        tokenRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        byte[] _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
+        tokenRequest.ContentLength = _byteVersion.Length;
+        Stream stream = tokenRequest.GetRequestStream();
+         stream.Write(_byteVersion, 0, _byteVersion.Length);
+        stream.Close();
+
+        try
+        {
+            // gets the response
+            WebResponse tokenResponse =  tokenRequest.GetResponse();
+            using (StreamReader reader = new StreamReader(tokenResponse.GetResponseStream()))
+            {
+                // reads response body
+                string responseText =  reader.ReadToEnd();
+
+                // converts to dictionary
+                Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                access_token = tokenEndpointDecoded["access_token"];
+                loginPage.gameObject.SetActive(false);
+                menuPage.gameObject.SetActive(true);
+            }
+        }
+        catch (WebException ex)
+        {
+            if (ex.Status == WebExceptionStatus.ProtocolError)
+            {
+                var response = ex.Response as HttpWebResponse;
+                if (response != null)
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        // reads response body
+                        string responseText =  reader.ReadToEnd();
+                    }
+                }
+
+            }
+        }
+    }
+    #endregion
+  
+    //TODO: Construct the email message to be sent
+    void constructEmailMessage(string to, string from, string subject, string bodyText,string _Access)
+    {
+        SmtpMail msg = new SmtpMail("TryIt");
+        EASendMail.SmtpClient osmtp = new EASendMail.SmtpClient();
+        try
+        {
+            SmtpServer oServer = new SmtpServer("smtp.gmail.com");
+            oServer.ConnectType = SmtpConnectType.ConnectSSLAuto;
+            oServer.Port = 587;
+            oServer.AuthType = SmtpAuthType.XOAUTH2;
+            oServer.Password = _Access;
+            oServer.User = userID;
+            msg.From = from;
+            msg.To = to;
+            msg.Subject = subject;
+            msg.TextBody = bodyText;
+            msg.ReplyTo = from;
+            osmtp.SendMail(oServer, msg);
+        }catch(Exception exp)
+        {
+            Debug.LogWarning(exp.Message);
+        }
+         //This way takes the users email and password. Not as safe as you would have to handle their info securely.
+        //SmtpClient _oSmtp = new SmtpClient();
+        //_oSmtp.Host = "smtp.gmail.com";
+        //_oSmtp.Port = 587;
+        //_oSmtp.Credentials = new System.Net.NetworkCredential(from, _Access) as ICredentialsByHost;
+        //_oSmtp.EnableSsl = true;
+        //MailMessage temp = new MailMessage(from, to, subject, bodyText);
+        //temp.ReplyTo = temp.From;
+        //_oSmtp.SendAsync(temp, "RUbella10");
+
+
+    }
+    private void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+    {
+        if (e.Cancelled || e.Error != null)
+        {
+            print("Email not sent: " + e.Error.ToString());
+
+            resultEmailSucess = false;
+            triggerResultEmail = true;
+        }
+        else
+        {
+            print("Email successfully sent.");
+
+            resultEmailSucess = true;
+            triggerResultEmail = true;
+        }
+    }
+    //Function to turn on the email canvas
+    private void switchCanvasForSendingEmail()
+    {
+        menuPage.gameObject.SetActive(false);
+        sendEmailPage.gameObject.SetActive(true);
+    }
+    //TODO:Construct a message and UI so you are able to send an Email
+    private void sendEmail()
+    {
+        if (access_token.CompareTo("") != 0)
+        {
+            bool missingField = false;
+            if (To.text.CompareTo("") == 0)
+            {
+                missingField = true;
+            }
+            if (From.text.CompareTo("") == 0)
+            {
+                missingField = true;
+            }
+
+            if (!missingField)
+            {
+                constructEmailMessage(To.text, From.text, Subject.text, Body.text, access_token);
+            }
+            else
+            {
+                Debug.Log("Invalid field");
+            }
+        }else
+        {
+            Debug.Log("Invalid Access Token");
+        }
+
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private static string randomDataBase64url(uint length)
+    {
+        RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        byte[] bytes = new byte[length];
+        rng.GetBytes(bytes);
+        return base64urlencodeNoPadding(bytes);
+    }
+
+    /// <summary>
+    /// Returns the SHA256 hash of the input string.
+    /// </summary>
+    /// <param name="inputStirng"></param>
+    /// <returns></returns>
+    private static byte[] sha256(string inputStirng)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes(inputStirng);
+        SHA256Managed sha256 = new SHA256Managed();
+        return sha256.ComputeHash(bytes);
+    }
+
+    /// <summary>
+    /// Base64url no-padding encodes the given input buffer.
+    /// </summary>
+    /// <param name="buffer"></param>
+    /// <returns></returns>
+    private static string base64urlencodeNoPadding(byte[] buffer)
+    {
+        string base64 = System.Convert.ToBase64String(buffer);
+
+        // Converts base64 to base64url.
+        base64 = base64.Replace("+", "-");
+        base64 = base64.Replace("/", "_");
+        // Strips padding.
+        base64 = base64.Replace("=", "");
+
+        return base64;
+    }
+
+    private static int GetRandomUnusedPort()
+    {
+        var listener = new TcpListener(IPAddress.Loopback, 0);
+        listener.Start();
+        var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+        listener.Stop();
+        return port;
+    }
+    private bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+     bool isOk = true;
+     // If there are errors in the certificate chain, look at each error to determine the cause.
+     if (sslPolicyErrors != SslPolicyErrors.None) {
+         for (int i=0; i<chain.ChainStatus.Length; i++) {
+             if (chain.ChainStatus [i].Status != X509ChainStatusFlags.RevocationStatusUnknown) {
+                 chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                 chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                 chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+                 chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                 bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+                 if (!chainIsValid) {
+                     isOk = false;
+                 }
+             }
+         }
+     }
+     return isOk;
+ }
+
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,375 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour {
+    // Use this for initialization
+
+    //List to hold all of the notifications
+    private ArrayList notificationArray;
+    private ArrayList shownArray;
+    //Value to hold index of the current group of notifications being shown to the user
+    private int currentShownIndexGroup;
+    //Data member to hold the message from the input field
+    private string storedMessage;
+    private bool filteringToggle;
+    [SerializeField]
+    private bool sortMessage;
+    [SerializeField]
+    string filter;
+    //Flag to determine whether the messages are sorted in ascending or descending order
+    [SerializeField]
+    private bool sortApplicationAsc;
+    //Max number of notifications to store
+    [SerializeField]
+    private int notificationNumber;
+   // Datamember to hold a reference to the input field in the scene
+    InputField inputText;
+    Button filterButton;
+    //Font for the notifications
+    string font;
+    [SerializeField]
+    bool timestampedNotfications;
+    [SerializeField]
+    GUIStyle style;
+
+    #region Predicates
+
+    //Comparer class that is used to sort an arraylist of strings in ascending order.
+    public class notificationMessageAscendingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            //Testing to see if the objects being passed in are notification structs.
+            if (!(x is Notification) || !(y is Notification))
+            {
+                return 0;
+            }
+            Notification a = (Notification)x;
+            Notification b = (Notification)y;
+            if(string.Compare(a.getApplicationString(), b.getApplicationString()) == 0)
+            {
+                return string.Compare(a.getMessageString(), b.getMessageString());
+
+            }else
+            {
+                return string.Compare(a.getApplicationString(), b.getApplicationString());
+            }
+
+        }
+    }
+
+    //Comparer class that is used to sort an arraylist of strings in descending order.
+    public class notificationMessageDescendingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            //Testing to see if the objects being passed in are notification structs.
+            if (!(x is Notification) || !(y is Notification))
+            {
+                return 0;
+            }
+            Notification a = (Notification)x;
+            Notification b = (Notification)y;
+            if (string.Compare(a.getApplicationString(), b.getApplicationString()) == 0)
+            {
+                return string.Compare(b.getMessageString(), a.getMessageString());
+
+            }
+            else
+            {
+                return string.Compare(b.getApplicationString(), a.getApplicationString());
+            }
+           
+        }
+    }
+    public class notifcationApplicationAscendingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            //Testing to see if the objects being passed in are notification structs.
+            if (!(x is Notification) || !(y is Notification))
+            {
+                return 0;
+            }
+            Notification a = (Notification)x;
+            Notification b = (Notification)y;
+
+            return string.Compare(a.getApplicationString(), b.getApplicationString());
+        }
+    }
+    public class notifcationApplicationDescendingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            //Testing to see if the objects being passed in are notification structs.
+            if (!(x is Notification) || !(y is Notification))
+            {
+                return 0;
+            }
+            Notification a = (Notification)x;
+            Notification b = (Notification)y;
+
+            return string.Compare(b.getApplicationString(), a.getApplicationString());
+        }
+    }
+    #endregion
+    //structure for the notifications. Contains a string to hold the name of the application and a string to hold the message from the application that should be displayed.
+
+    protected struct Notification
+    {
+        string application;
+        string message;
+        public Notification(string _application = null, string _message = null)
+        {
+            application = _application;
+            message = _message;
+        }
+
+        //Getters and setters for the application and message strings.
+        public string getApplicationString() { return application; }
+        public void setApplicationString(string _application) { application = _application; }
+        public string getMessageString() { return message; }
+        public void setMessageString(string _message) { message = _message; }
+    };
+    
+    //Function that is executed when a onEndEdit event is sent from the Input field.
+    //It adds a new notification based on the text inside the input field.
+    void lockInput(string args0)
+    {
+        if (inputText.text.Length > 0)
+        {
+            storedMessage = inputText.text;
+            if (timestampedNotfications)
+            {
+                AddNewNotificationTimeStamp("InputField", storedMessage);
+            }
+            else
+            {
+                AddNewNotification("InputField", storedMessage);
+            }
+            inputText.text = "";
+        }
+    }
+    //Ensures that there is only one instance of this script.
+    public static NewBehaviourScript _instance = null;
+    private void Awake()
+    {
+     
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+  
+    //The event listener is bound to the lockInput onEndEdit event.
+    void Start () {
+        
+
+        notificationArray = new ArrayList();
+        shownArray = new ArrayList();
+        //inputText = gameObject.GetComponent<InputField>();
+        //var se = new InputField.SubmitEvent();
+        //se.AddListener(lockInput);
+        //inputText.onEndEdit = se;
+        style = new GUIStyle();
+        filterButton = GameObject.FindObjectOfType<Button>();
+        var see = new Button.ButtonClickedEvent();
+        see.AddListener(startFiltering);
+        filterButton.onClick = see;
+
+        filteringToggle = false;
+    }
+
+    private void startFiltering()
+    {
+        shownArray.Clear();
+        if (!filteringToggle)
+        {
+            
+            for (int i = 0; i < notificationArray.Count; i++)
+            {
+                Notification temp = (Notification)notificationArray[i];
+                if (!filter.Equals(temp.getApplicationString(), StringComparison.OrdinalIgnoreCase))
+                {
+                    shownArray.Add(temp);
+                }
+            }
+            filteringToggle = !filteringToggle;
+        }else
+        {
+            shownArray = (ArrayList)notificationArray.Clone();
+            filteringToggle = !filteringToggle;
+
+        }
+        if (sortApplicationAsc)
+        {
+            shownArray.Sort(new notifcationApplicationAscendingComparer());
+
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageAscendingComparer());
+            }
+           
+        }
+        else
+        {
+            shownArray.Sort(new notifcationApplicationDescendingComparer());
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageDescendingComparer());
+            }
+           
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        //Implement a solution for switching between the shown notifications.
+        
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                int diff = notificationArray.Count - currentShownIndexGroup;
+                if (diff>10)
+                {
+                    if (currentShownIndexGroup != 100)
+                    {
+
+                        currentShownIndexGroup += 10;
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                if (currentShownIndexGroup != 0)
+                {
+                    currentShownIndexGroup -= 10;
+                }
+            }
+        
+    }
+    //This function adds a new notification and sorts the arraylist based on the sorting flag.
+    public void AddNewNotification(string _application, string _message )
+    {
+
+        if (notificationArray.Count < notificationNumber)
+        {
+            Notification newMessage = new Notification();
+            newMessage.setApplicationString(_application);
+            newMessage.setMessageString(_message);
+            notificationArray.Add(newMessage);
+            Notification otherMessage = newMessage;
+            shownArray.Add(otherMessage);
+        }
+        if (sortApplicationAsc)
+        {
+            shownArray.Sort(new notifcationApplicationAscendingComparer());
+
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageAscendingComparer());
+            }
+
+        }
+        else
+        {
+            shownArray.Sort(new notifcationApplicationDescendingComparer());
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageDescendingComparer());
+            }
+
+        }
+    }
+    //This function adds a new notification with a timestamp and sorts the arraylist based on the sorting flag.
+    public void AddNewNotificationTimeStamp(string _application, string _message)
+    {
+
+        if (notificationArray.Count < notificationNumber)
+        {
+            Notification newMessage = new Notification();
+            string timestamped = _application + " " + System.DateTime.Now;
+            if (string.Compare(_message, "Time") == 0)
+            {
+                newMessage.setApplicationString(timestamped);
+                newMessage.setMessageString(_message);
+                notificationArray.Add(newMessage);
+                Notification otherMessage = newMessage;
+                shownArray.Add(otherMessage);
+            }else {
+                newMessage.setApplicationString("Filter");
+                newMessage.setMessageString(_message);
+                notificationArray.Add(newMessage);
+                Notification otherMessage = newMessage;
+                shownArray.Add(otherMessage);
+            }
+            
+        }
+
+        if (sortApplicationAsc)
+        {
+            shownArray.Sort(new notifcationApplicationAscendingComparer());
+
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageAscendingComparer());
+            }
+            else
+            {
+                shownArray.Sort(new notificationMessageDescendingComparer());
+            }
+
+        }
+        else
+        {
+            shownArray.Sort(new notifcationApplicationDescendingComparer());
+            if (sortMessage)
+            {
+                shownArray.Sort(new notificationMessageDescendingComparer());
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+    //This function removes a notification from the array list
+    public void RemoveNotification(int index)
+    {
+        if(index < notificationArray.Count&&index>=0)
+        {
+            notificationArray.RemoveAt(index);
+        }
+      
+    }    
+    void OnGUI()
+    {
+
+        
+        for (int i = currentShownIndexGroup; i < currentShownIndexGroup + 10; i++)
+            {
+                if (i < shownArray.Count)
+                {
+                    Notification toDisplay = (Notification)shownArray[i];
+                    string pulledTogether = toDisplay.getApplicationString() + ": " + toDisplay.getMessageString();
+                    GUI.Label(new Rect(0, (i % 10) * 10, 300, 20), pulledTogether, style);
+               
+                }
+            }
+
+        }
+    
+        
+}
+
